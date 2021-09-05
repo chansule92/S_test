32e09d9aab5ee60c39988a1b9d4fda7aa8ade20b
chansule92 <57983744+chansule92@users.noreply.github.com>
2021-09-06 08:22:39 +0900

    Add files via upload

--- science/config/settings.py
+++ science/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'science.apps.ScienceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
